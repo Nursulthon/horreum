7c8d357d0e26c72e338a9940300f0a51cff65531
1mGoodDev <sulthon.elmanaar@gmail.com>
2026-03-23 06:04:35 +0700

    Model and relation database

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,64 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Psy\Util\Str;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'about',
+        'price',
+        'category_id',
+        'is_popular',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function warehouses()
+    {
+        return $this->belongsToMany(Warehouse::class, 'warehouse_products')
+            ->withPivot('stock')
+            ->withTimestamps();
+    }
+
+    public function merchants()
+    {
+        return $this->belongsToMany(Merchant::class, 'merchant_products')
+            ->withPivot('stock')
+            ->withTimestamps();
+    }
+
+    public function transaction()
+    {
+        return $this->hasMany(TransactionProduct::class);
+    }
+
+    public function getWarehouseProductStock()
+    {
+        return $this->warehouses()->sum('stock');
+    }
+
+    public function getMerchantProductStock()
+    {
+        return $this->merchants()->sum('stock');
+    }
+
+    public function getThumbnailUrlAttribute($value)
+    {
+        if (!$value)
+            {
+                return null;
+            }
+
+        return url(Storage::url($value));
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,38 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'photo',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +48,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function merchant()
+    {
+        return $this->hasOne(Merchant::class, 'keeper_id', 'id');
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        return url(Storage::url($value));
     }
 }
