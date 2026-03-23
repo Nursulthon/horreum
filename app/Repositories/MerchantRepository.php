b89468f05a72b4072f1e7398ebeeaf44d7a51964
1mGoodDev <sulthon.elmanaar@gmail.com>
2026-03-23 23:52:39 +0700

    Access data layer: - CRUD with product.category data - getByKeeperId function (take merchant data by keeperId)

---
+++ app/Repositories/MerchantRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Merchant;
+
+class MerchantRepository {
+
+    public function getAll(array $fields)
+    {
+        return Merchant::select($fields)
+            ->with('keeper_id', 'products.category')
+            ->latest()
+            ->paginate(10);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return Merchant::select($fields)
+            ->with('keeper_id', 'products.category')
+            ->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+       return Merchant::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $merchant = Merchant::findOrFail($id);
+        return $merchant->update($data);
+    }
+
+    public function delete(int $id)
+    {
+        $merchant = Merchant::findOrFail($id);
+        $merchant->delete();
+    }
+
+    public function getByKeeperId(int $keeperId, array $fields)
+    {
+        return Merchant::select($fields)
+            ->where('keeper_id', $keeperId)
+            ->with('product.category')
+            ->findOrFail();
+    }
+}
