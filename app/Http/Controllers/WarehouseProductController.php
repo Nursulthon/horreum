c875d0162ae37c2a4a24014090152b3a02f76eb6
1mGoodDev <sulthon.elmanaar@gmail.com>
2026-03-23 22:16:00 +0700

    WarehouseProductController: - Depedency injection - attach & detach function - update function

--- app/Http/Controllers/WarehouseProductController.php
+++ app/Http/Controllers/WarehouseProductController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WarehouseProductRequest;
+use App\Services\WarehouseService;
 use Illuminate\Http\Request;
 
 class WarehouseProductController extends Controller
 {
-    //
+    private $warehouseService;
+
+    public function __construct(WarehouseService $warehouseService)
+    {
+        $this->warehouseService = $warehouseService;
+    }
+
+    public function attach(int $warehouseId, WarehouseProductRequest $request)
+    {
+        $data = $request->validated();
+
+        $this->warehouseService->attachProduct(
+            $warehouseId,
+            $data['product_id'],
+            $data['stock']
+        );
+
+        return response()->json([
+            'message' => 'Product attached successfully.'
+        ]);
+    }
+
+    public function detach(int $warehouseId, int $productId)
+    {
+        $this->warehouseService->detachProduct($warehouseId, $productId);
+
+        return response()->json([
+            'message' => 'Product removed successfully.'
+        ]);
+    }
+
+    public function update(int $warehouseId, WarehouseProductRequest $request)
+    {
+        $data = $request->validated();
+
+        $warehouseProductUpdatedStock = $this->warehouseService->updateProductStock(
+            $warehouseId,
+            $data['product_id'],
+            $data['stock'],
+        );
+
+        return response()->json([
+            'message' => 'Stock updated successfully',
+            'data' => $warehouseProductUpdatedStock
+        ]);
+    }
 }
